89486f9f9699c2f59671800e4c3c597ee315de97
Dmitry <dmitr5.11.1997@gmail.com>
2019-09-27 19:43:07 +0300

    Delete assimp dependency for now and replace it with tinyobjloader as it's more lightweight and will suffice all the requirements for now.

--- src/model.cpp
+++ src/model.cpp
@@ -1,18 +1,17 @@
 #include "model.h"
 
-#include <assimp/Importer.hpp>
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
 
 namespace model_utils
 {
     model load(std::string_view file_name) noexcept
     {
-        Assimp::Importer importer;
         return model{ {}, {} };
     }
 }
 
-
-model::model(std::vector<vertex>&& vertices, std::vector<size_t>&& indices)
+model::model(std::vector<vertex>&& vertices, std::vector<size_t>&& indices) noexcept
     : vertices_(std::move(vertices))
     , indices_(std::move(indices)) {}
 
